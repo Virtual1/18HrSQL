f4e4c99123c62ee7b16f068f2d0126564bd5eafe
Virtual1 <jessica@virtualangels.co.za>
2018-07-18 15:48:12 +0200

    Loaded solution files from course
    Loaded solution files from course

---
+++ Tom O Solution/Main.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Main : System.Web.UI.Page
+{
+
+    protected void chkDetailsView_CheckedChanged(object sender, EventArgs e)
+    {
+        if (chkDetailsView.Checked)
+            dvPanel.Visible = true;
+        else
+            dvPanel.Visible = false;
+    }
+
+    protected void dvEmployees_ItemDeleted(object sender, DetailsViewDeletedEventArgs e)
+    {
+        gvSales.DataBind();//runs after item is removed from details view
+        gvSummary.DataBind();//added to show how to update Summary Div after deletion of record from dvEmployees
+    }
+
+    protected void gvSales_RowDeleted(object sender, GridViewDeletedEventArgs e)
+    {
+        gvSummary.DataBind();//refreshes summary grid view after a record is deleted from from gvSales
+    }
+
+    protected void gvSales_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+    {
+        gvSummary.DataBind();//refreshes summary grid view after a record is updated
+    }
+}
